e397c0e2a56073fc929c7ceaaaaa5593c259a49c
gaoqianhui <1240165896@qq.com>
2019-10-23 10:39:44 +0800

    no message

--- service/routes/error.js
+++ service/routes/error.js
@@ -1,23 +1,26 @@
-const router = require("koa-router")();
-const fs = require("fs"),
-  path = require("path");
-router.prefix("/errors");
+const router = require('koa-router')();
+const fs = require('fs'),
+	path = require('path');
+router.prefix('/errors');
 router.post(
-  "/",
-  (() => {
-    return async ctx => {
-      ctx.status = 204;
-      await fs.writeFile(
-        path.resolve(__dirname, "../log/dev.error.log"),
-        `\n ${new Date()}：${ctx.request.body};`,
-        { flag: "a", encoding: "utf-8", mode: "0666" },
-        async err => {
-          if (err) {
-            return console.log(err);
-          }
-        }
-      );
-    };
-  })()
+	'/',
+	(() => {
+		return async ctx => {
+			ctx.status = 204;
+			let filePath = ctx.request.body.includes('http://120.78.169.181')
+				? 'prod.error.log'
+				: 'dev.error.log';
+			await fs.writeFile(
+				path.resolve(__dirname, filePath),
+				`\n ${new Date()}：${ctx.request.body};`,
+				{ flag: 'a', encoding: 'utf-8', mode: '0666' },
+				async err => {
+					if (err) {
+						return console.log(err);
+					}
+				}
+			);
+		};
+	})()
 );
 module.exports = router;
